812a9e74fc51396ea23adf9d64513a336eed1202
sbonisso <sbonisso@ucsd.edu>
2015-05-23 13:29:35 -0700

    added bin/cluster_eval

--- lib/cluster_eval.rb
+++ lib/cluster_eval.rb
@@ -1,5 +1,10 @@
 require "cluster_eval/version"
 
 module ClusterEval
-  # Your code goes here...
+  
+  def self.confusion_matrix(clust_a, clust_b)
+    
+    {:a => 0, :b => 0, :c => 0, :d => 0}
+  end
+
 end
