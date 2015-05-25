93f17fe985ed58232c0bc44e55bb069036ce3493
sbonisso <sbonisso@ucsd.edu>
2015-05-25 00:39:55 -0700

    added temp cout statement

--- ext/ConfusionMatrix.cpp
+++ ext/ConfusionMatrix.cpp
@@ -79,6 +79,7 @@ void ConfusionMatrix::compute_confusion_matrix() {
     c_ = v1.second;
     d_ = v2.first;
     b_ = ((n*(n-1))/2) - (a_+c_+d_);
+    std::cout<<a_<<"\t"<<b_<<"\t"<<c_<<"\t"<<d_<<std::endl;
 }
 /**
  * return Rand index: (a+b)/(a+b+c+d)
